3b4efc2d18d716130ea1db72dbb804585dd7d927
sfzsjx <2590667609@qq.com>
2019-05-12 17:03:04 +0800

    spring-cloud 服务配置 和 网关API

--- hello-spring-cloud-web-admin-feign/src/main/java/com/sfz/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/sfz/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
@@ -3,13 +3,15 @@ package com.sfz.hello.spring.cloud.web.admin.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@SpringBootApplication
+@EnableHystrixDashboard
 public class WebAdminFeignApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WebAdminFeignApplication.class, args);
+        SpringApplication.run(WebAdminFeignApplication.class,args);
     }
 }
--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/sfz/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/sfz/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.sfz.hello.spring.cloud.web.admin.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,12 @@ public class AdminService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String sayHi(String message){
         return restTemplate.getForObject("http://HELLO-SPRING-CLOUD-SERVICE-ADMIN/hi?message=" + message, String.class);
+    }
+
+    public String hiError(String message){
+        return String.format("hi your message is %s but request error ",message);
     }
 }
